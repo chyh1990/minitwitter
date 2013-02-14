14837d6ca0878f4c48bc5fa1274588870ca464a1
Chen Yuheng <chyh1990@gmail.com>
2013-02-14 18:22:07 +0800

    init rails 3.2.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minitwitter::Application.config.secret_token = '9fb9b373639f57ac4c45ab8a2e34ca3a30a991e23fc6c16e35c032e7c1ad557ac1b57ea7607bbfe46b6afb1ca557cf87f2f629d69dd0cc8c1e21d559d7490844'
